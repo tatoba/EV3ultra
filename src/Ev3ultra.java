f6c52c85a13336e45d2a4afcd74fb72fddd0c5ac
faize_000 <faize_000@133.54.245.94>
2015-05-20 19:05:43 +0900

    研究用のプログラム作成

---
+++ src/Ev3ultra.java
@@ -0,0 +1,33 @@
+import lejos.hardware.motor.Motor;
+import lejos.hardware.port.SensorPort;
+import lejos.hardware.sensor.EV3UltrasonicSensor;
+
+public class Ev3ultra {
+
+	static int a;
+
+	public static void main(String[] args) {
+
+		// TODO 自動生成されたメソッド・スタブ
+//		getDistanceMode a;
+		EV3UltrasonicSensor us1 = new EV3UltrasonicSensor(SensorPort.S4);
+		
+		Motor.B.setSpeed(100);// 2 RPM
+        Motor.C.setSpeed(100);
+		
+		us1.enable();
+		us1.getDistanceMode();
+		
+		
+		
+		
+
+		us1.close();
+	
+	}
+
+
+	
+	
+}
+
